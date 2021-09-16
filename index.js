aadd90d17489c2323f9edcd3a284599c36cb1dcb
cpandit <chirag.pandit@here.com>
2021-09-16 12:29:25 +0530

    Updating to IML APi

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
-fetch('http://example.com/movies.json')
+fetch('https://interactive-dev-eu-west-1.api-gateway.sit.ls.hereapi.com/interactive/v1' +
+    '/catalogs/hrn:here-dev:data::olp-here-ci-mp-p:655f88-f71d9e728' +
+    '/layers/b9c943a' +
+    '/iterate?apiKey=tRdgyeKm9ciQb6Z4fC-zU0zolHSQ37fAKZ_BMJbOjM0')
     .then(response => response.json())
     .then(data => console.log(data));
 
